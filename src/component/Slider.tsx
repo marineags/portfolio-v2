1d0b4cdb16faec8f1bfd96ddef8c59a2749bab61
marineags <agsmarine23@gmail.com>
2026-05-11 17:19:45 +0200

    update

--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -14,7 +14,7 @@ const destinations: Destination[] = [
     title: "DATAVIZ",
     desc: "DataViz est une application interactive de visualisation de données permettant d’explorer des statistiques et graphiques de façon claire et moderne.",
     image:
-      "src/images/dataviz.png",
+      "./images/dataviz.png",
     link: "https://dataviz-navy.vercel.app",
   },
   {
@@ -22,7 +22,7 @@ const destinations: Destination[] = [
     title: "QUIZZY",
     desc: "Quizy est une application de quiz interactive et ludique permettant de tester ses connaissances à travers différentes questions dans une interface moderne et dynamique.",
     image:
-      "src/images/quizzy.png",
+      "./images/quizzy.png",
     link: "https://quizz-pi-plum.vercel.app",
   },
   {
@@ -30,7 +30,7 @@ const destinations: Destination[] = [
     title: "ADAOPTE",
     desc: "Adaopte est une application web dédiée à l’adoption d’animaux, permettant de découvrir différents profils d’animaux dans une interface moderne, intuitive et pensée pour faciliter les rencontres entre futurs adoptants et refuges.",
     image:
-      "src/images/ADAOPTE.png",
+      "./images/ADAOPTE.png",
     link: "https://adaopte-tau.vercel.app",
   },
   
@@ -39,7 +39,7 @@ const destinations: Destination[] = [
     title: "EXO FLEXBOX",
     desc: "Cette application est un exercice de développement front-end mettant en avant la création d’un affichage interactif et dynamique avec une interface moderne et responsive, conçu pour pratiquer la logique et l’intégration web.",
     image:
-      "src/images/Capture d’écran 2026-05-07 à 15.40.35.png",
+      "./images/Capture d’écran 2026-05-07 à 15.40.35.png",
     link: "https://exercice-1-produire-un-affichage-en.vercel.app",
   },
 ];
